c71c12a70f2b71b078cf28be5550ff397f4aa38b
NIGHTZIN <hatsunemikutoro@gmail.com>
2026-02-18 20:02:55 -0300

    Criar planilha e preencher planilha adicionados e com exemplos

--- cli.py
+++ cli.py
@@ -1,14 +1,40 @@
-from core import criarPlanilha
+from core import criarPlanilha, preencherPlanilha
 
-data = input("Escreva os dados separando por VIRGULA: ")
-valores = [v.strip() for v in data.split(",")]
+while True:
+    print("===== EXCEL MANAGER CLI VERSION =====")
+    print("1. CRIAR PLANILHA")
+    print("2. PREENCHER PLANILHA")
+    opt = int(input("> "))
 
-try:
-    colunas = int(input("Quantas colunas deve ter: "))
+    if opt == 1:
+        try:
+            data = input("Escreva os dados separando por VIRGULA ( ex: nome,idade,joao,maria,32,32): ")
+            valores = [v.strip() for v in data.split(",")]
 
-    nome_arquivo = input("Qual o nome desejado? ( Vazio = Resultado.xlsx :")
-except ValueError:
-    print("Número invalido")
-    exit()
+            colunas = int(input("Quantas colunas deve ter: "))
 
-criarPlanilha(valores, colunas, nome_arquivo)
+            nome_arquivo = input("Qual o nome desejado? ( Vazio = Resultado.xlsx ):")
+        except ValueError:
+            print("Número invalido")
+            exit()
+
+        criarPlanilha(valores, colunas, nome_arquivo)
+        break
+    elif opt == 2:
+        try:
+            path = input("Caminho do arquivo: ")
+
+            data = input("Escreva os dados separando por VIRGULA ( ex: joao,32, maria, 32): ")
+            valores = [v.strip() for v in data.split(",")]
+
+            colunas = int(input("Quantas colunas deve ter: "))
+
+            linha_inicial = 2
+
+            preencherPlanilha(path, valores, colunas, linha_inicial)
+            break
+        except ValueError:
+            print("Valores invalidos")
+            exit()
+    else:
+        print("Opções invalidas")
